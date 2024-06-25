a5487bd6c78b96cc1b82c9a4b5c67a42d459f906
scott <scottysora@gmail.com>
2024-06-25 16:09:19 -0400

    createpost function and logout function

--- posts/posts.js
+++ posts/posts.js
@@ -20,3 +20,72 @@ window.onload = () => {
   onDropdownSort();
   dropdownSortPosts.onchange = onDropdownSort;
 };
+
+const wait = (delay = 0) =>
+  new Promise((resolve) => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) =>
+  ((typeof elementOrSelector === "string"
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+  ).style.display = visible ? "block" : "none");
+
+setVisible(".page", false);
+setVisible("#loading", true);
+
+document.addEventListener("DOMContentLoaded", () =>
+  wait(1000).then(() => {
+    setVisible(".page", true);
+    setVisible("#loading", false);
+  })
+);
+
+// Show or hide button that scrolls to the top of page
+window.onscroll = () => {
+  scrollFunction();
+};
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnScrollToTop.style.display = "block";
+  } else {
+    btnScrollToTop.style.display = "none";
+  }
+};
+
+// logout button
+btnLogOut.onclick = () => {
+  logout();
+};
+
+// when button is clicked, create a post using value from form
+btnSubmit.onclick = function(event) {
+  event.preventDefault();
+  createPost(newPost.value);
+};
+
+// function for creating a new post
+function createPost(_content) {
+  fetch(`${apiBaseURL}/api/posts`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer " + bearerToken.token,
+    },
+    body: JSON.stringify({
+      text: _content,
+    }),
+    redirect: "follow",
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // test
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      location.reload();
+    });
+}
+
