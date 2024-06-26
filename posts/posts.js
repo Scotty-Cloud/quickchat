83d7d71cbc22e12fa85f17dfa9ad727f5a5a937b
scott <scottysora@gmail.com>
2024-06-26 00:33:17 -0400

    add like and unlike function & side menu drop down

--- posts/posts.js
+++ posts/posts.js
@@ -174,3 +174,179 @@ async function onDropdownSort() {
   }
 }
 
+// This is for generatig random number
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function for displaying all posts
+function displayAllPosts(
+  _username,
+  _date,
+  _text,
+  _numLikes,
+  _ownPost,
+  _valueID,
+  _isLiked,
+  _unlikeID
+) {
+  let ownPost = ""; //check if post owner so delete button appears
+  if (_ownPost) {
+    ownPost = `<button class="btnDelete" id="${_valueID}"><i class="fas fa-trash trash" style="color: #df4e4e;"></i></button>`;
+  } else {
+    ownPost = "";
+  }
+
+  //check if post is liked or not to display appropriate button
+  let isLiked = "";
+  if (_isLiked) {
+    isLiked = `<div class="post-activity-link btnLike clicked" id="${_unlikeID}" data-value="Liked">
+                    <i class="fas fa-heart"></i><span>Unlike</span>
+                  </div>`;
+  } else {
+    isLiked = `<div class="post-activity-link btnLike" id="${_valueID}" data-value="Unliked">
+                    <i class="fas fa-heart"></i><span>Like</span>
+                  </div>`;
+  }
+
+  const randomNumber = Math.floor(Math.random() * 150);
+  const randomRepost = Math.floor(Math.random() * 40);
+
+  displayPosts.innerHTML += `
+      <div class="post">
+                
+                <div class="post-author">
+                  
+                    <img src="../images/defaultUser.png" alt="Default user">
+                    <div>
+                        <h1>${_username}</h1>
+                        <small>${_date} </small>
+                    </div>
+                </div>
+                ${ownPost}
+                <p>
+                ${_text}
+                </p>
+
+                <div class="post-stats">
+                    <div>
+                        <span class="liked-users"> ${_numLikes} likes </span>
+                    </div>
+                    <div>
+                        <span> ${randomNumber} comments || ${randomRepost} Repost </span>
+                    </div>
+                </div>
+
+                <div class="post-activity">
+                    <div>
+                        <img src="../images/defaultUser.png" alt="" class="post-activity-user-icon">
+                    </div>
+                    ${isLiked}    
+                    <div class="post-activity-link">
+                        <i class="fas fa-comment"></i>
+                        <span>Comment</span>
+                    </div>
+                    <div class="post-activity-link">
+                        <i class="fas fa-share"></i>
+                        <span>Share</span>
+                    </div>
+                    <div class="post-activity-link">
+                        <i class="fas fa-paper-plane"></i>
+                        <span>Send</span>
+                    </div>
+
+                </div>
+            </div>
+      `;
+}
+
+// like post function
+async function likePost(_postID) {
+  try {
+    const response = await fetch(`${apiBaseURL}/api/likes`, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + bearerToken.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        postId: _postID,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    console.log("Post liked."); //test
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// unlike post function
+async function unlikePost(_postID) {
+  try {
+    const response = await fetch(`${apiBaseURL}/api/likes/${_postID}`, {
+      method: "DELETE",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + bearerToken.token,
+      },
+    });
+    const data = await response.json();
+    console.log(data); //test
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// delete post function
+async function deletePost(_postID) {
+  try {
+    const response = await fetch(`${apiBaseURL}/api/posts/${_postID}`, {
+      method: "DELETE",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + bearerToken.token,
+      },
+    });
+    const data = await response.json();
+    console.log(data); //test
+    console.log("Post deleted."); //test
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// this code is for the side menu to work properly
+const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+// local storage
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
